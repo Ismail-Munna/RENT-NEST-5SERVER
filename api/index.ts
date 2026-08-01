63edab3cbc4743a89068a054d46b8e6893797134
Ismail Munna <ismailhossainmunna8@gmail.com>
2026-08-01 20:16:26 +0600

    fix: resolve deployment configurations and build setup

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,4 @@
 import "dotenv/config";
-import serverless from "serverless-http";
 import app from "../src/app";
-import { prisma } from "../src/lib/prisma";
 
-const handler = serverless(app);
-let isPrismaConnected = false;
-
-const connectPrisma = async () => {
-  if (isPrismaConnected) return;
-  await prisma.$connect();
-  isPrismaConnected = true;
-};
-
-export default async (event: any, context: any) => {
-  await connectPrisma();
-  return handler(event, context);
-};
+export default app;
